55d6bb2f4de5986ecd7b1f3cd2518641882a1f25
Sagar Patil <sagarreddypatil@gmail.com>
2020-12-20 11:28:23 -0500

    Almost perfect launch. Need to decrease node finish tolerance.

--- launch.py
+++ launch.py
@@ -20,6 +20,7 @@ apoapsis = conn.add_stream(getattr, vessel.orbit, "apoapsis_altitude")
 latitude = conn.add_stream(getattr, vessel.flight(), "latitude")
 longitude = conn.add_stream(getattr, vessel.flight(), "longitude")
 throttle = conn.add_stream(getattr, vessel.control, "throttle")
+time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, "time_to_apoapsis")
 
 stage_one_fuel = conn.add_stream(
     vessel.parts.with_title("Ghidorah K1-180 Tank")[0].resources.amount, "LiquidFuel"
@@ -40,24 +41,36 @@ runmode_names = [
     "Gravity Turn",
     "Stage Seperation",
     "Burn till target apoapsis",
+    "Coast to Space",
     "Coast to Circularization",
     "Circularization",
+    "Circularization Fine-Tuning",
 ]
 
 T0 = ut() + 10
+circularization_node = None
+node_dv = None
+burn_time = -1
 
 print("\n")
 
 while runmode != -1:
+    print(
+        f"Run mode: {runmode_names[runmode]}\t-\tT: {round(ut() - T0, 1)}                  ",
+        end="\r",
+    )
+
     if runmode == 0:
         if ut() >= T0:
             vessel.control.activate_next_stage()
             vessel.auto_pilot.engage()
             vessel.auto_pilot.target_pitch_and_heading(90, 90)
             runmode = 1
+            continue
     if runmode == 1:
         if altitude() > turn_start_altitude:
             runmode = 2
+            continue
     if runmode == 2:
         frac = (altitude() - turn_start_altitude) / (
             turn_end_altitude - turn_start_altitude
@@ -67,6 +80,7 @@ while runmode != -1:
 
         if altitude() >= turn_end_altitude:
             runmode = 4
+            continue
 
         if not first_stage_seperated and (
             stage_one_fuel() / stage_one_initial_fuel <= 0.25
@@ -77,17 +91,69 @@ while runmode != -1:
             vessel.control.throttle = 1.0
             runmode = 3
             first_stage_sep_time = ut()
+            continue
     if runmode == 3:
         if ut() >= first_stage_sep_time + 3:
             vessel.control.activate_next_stage()
             first_stage_seperated = True
             runmode = 2
+            continue
     if runmode == 4:
-        ideal_throttle = max(min((target_orbit - apoapsis()) / 10, 100), 40)
+        ideal_throttle = max(min((target_orbit - apoapsis()) / 1000, 1), 0.4)
         if abs(throttle() - ideal_throttle) > 0.5:
             vessel.control.throttle = ideal_throttle
         if apoapsis() >= target_orbit:
             vessel.control.throttle = 0
             runmode = 5
+            continue
+    if runmode == 5:
+        if apoapsis() < target_orbit:
+            vessel.control.throttle = 0.4
+        if (apoapsis() >= target_orbit) and (throttle() > 0):
+            vessel.control.throttle = 0
+        if altitude() >= 70000:
+            vessel.control.activate_next_stage()
 
-    print(f"Run mode: {runmode_names[runmode]}\t-\tT: {round(ut() - T0, 3)}", end="\r")
+            mu = vessel.orbit.body.gravitational_parameter
+            r = vessel.orbit.apoapsis
+            a1 = vessel.orbit.semi_major_axis
+            a2 = r
+            v1 = math.sqrt(mu * ((2.0 / r) - (1.0 / a1)))
+            v2 = math.sqrt(mu * ((2.0 / r) - (1.0 / a2)))
+            delta_v = v2 - v1
+            circularization_node = vessel.control.add_node(
+                ut() + vessel.orbit.time_to_apoapsis, prograde=delta_v
+            )
+            node_dv = conn.add_stream(
+                getattr, circularization_node, "remaining_delta_v"
+            )
+
+            F = vessel.available_thrust
+            Isp = vessel.specific_impulse * 9.82
+            m0 = vessel.mass
+            m1 = m0 / math.exp(delta_v / Isp)
+            flow_rate = F / Isp
+            burn_time = (m0 - m1) / flow_rate
+
+            vessel.auto_pilot.reference_frame = circularization_node.reference_frame
+            vessel.auto_pilot.target_direction = (0, 1, 0)
+            vessel.auto_pilot.wait()
+            conn.space_center.warp_to(ut() + time_to_apoapsis() - (burn_time / 2) - 5)
+
+            runmode = 6
+            continue
+    if runmode == 6:
+        if time_to_apoapsis() - (burn_time / 2) < 0:
+            vessel.control.throttle = 1.0
+            runmode = 7
+            continue
+    if runmode == 7:
+        if node_dv() < 100 and throttle() > 0.4:
+            vessel.control.throttle = 0.4
+            runmode = 8
+            continue
+    if runmode == 8:
+        if node_dv() < 5:
+            vessel.control.throttle = 0
+            runmode = -1
+            continue
